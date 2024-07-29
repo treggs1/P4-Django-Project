eab0c02bb091ba5933ae787d1c75a221ad040e9b
Paul Treggiden <treggs1@gmail.com>
2024-07-29 07:51:08 +0000

    Add comment to views

--- contact/views.py
+++ contact/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect
 from .models import Contact
 
 def contact_form(request):
+    """ A view to display contact page and handle form data """
     if request.method == 'POST':
         email = request.POST.get('email')
         message = request.POST.get('message')
@@ -12,4 +13,5 @@ def contact_form(request):
     return render(request, 'contact/contact_form.html')
 
 def contact_success(request):
+    """ A view to display contact success page """
     return render(request, 'contact/contact_success.html')
